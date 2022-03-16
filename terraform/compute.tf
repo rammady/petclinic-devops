b762bf2c69ebad2100641b21db0eb85debe11a18
Ramesh Maddheshiya <ramjss08@gmail.com>
2022-03-16 12:37:06 +0530

    compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -19,10 +19,10 @@ resource "aws_instance" "jenkins-instance" {
   ami             = "${data.aws_ami.amazon-linux-2.id}"
   instance_type   = "t2.medium"
   key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
+  vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
+  name            = "${var.name}"
   user_data = "${file("install_jenkins.sh")}"
 
   associate_public_ip_address = true
@@ -30,7 +30,7 @@ resource "aws_instance" "jenkins-instance" {
     Name = "Jenkins-Instance"
   }
 }
-
+# Jebkins Instance - EC2-1
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
@@ -60,4 +60,100 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
 
 output "jenkins_ip_address" {
   value = "${aws_instance.jenkins-instance.public_dns}"
+}
+
+#Ansible Instance -EC2-2
+resource "aws_instance" "ansible-instance" {
+  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type   = "t2.medium"
+  key_name        = "${var.keyname}"
+  vpc_id          = "${aws_vpc.development-vpc.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_ansible.id}"]
+  subnet_id          = "${aws_subnet.private-subnet-1.id}"
+  name            = "${var.name}"
+  user_data = "${file("install_ansibledocker.sh")}"
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "Ansible-Instance"
+  }
+}
+
+resource "aws_security_group" "sg_allow_ssh_ansible" {
+  name        = "allow_ssh_jenkins"
+  description = "Allow SSH and Ansible inbound traffic"
+  vpc_id      = "${aws_vpc.development-vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+output "jenkins_ip_address" {
+  value = "${aws_instance.ansible-instance.public_dns}"
+}
+
+#Dockerhost Instance -EC2-3
+resource "aws_instance" "dockerhost-instance" {
+  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type   = "t2.medium"
+  key_name        = "${var.keyname}"
+  vpc_id          = "${aws_vpc.development-vpc.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_dockerhost.id}"]
+  subnet_id          = "${aws_subnet.private-subnet-2.id}"
+  name            = "${var.name}"
+  user_data = "${file("install_docker.sh")}"
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "Dockerhost-Instance"
+  }
+}
+
+resource "aws_security_group" "sg_allow_ssh_dockerhost" {
+  name        = "allow_ssh_jenkins"
+  description = "Allow SSH and Dockerhost inbound traffic"
+  vpc_id      = "${aws_vpc.development-vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8082
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+output "jenkins_ip_address" {
+  value = "${aws_instance.dockerhost-instance.public_dns}"
 }
